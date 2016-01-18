ead15888f3779d79e7c870e5e4498b939eaebb95
philmphoenix <jpholzmann@gmail.com>
2016-01-17 18:02:00 -0700

    Adds surveyId to segment the socket.

--- public/admin.js
+++ public/admin.js
@@ -3,8 +3,10 @@ const socket = io();
 const connectionCount = document.getElementById('connection-count');
 const statusMessage = document.getElementById('status-message');
 const choices = document.getElementById('survey-choices')
+const countedVotes = document.getElementById('results');
+const surveyId = document.getElementById('survey-id').innerText;
 
-socket.on('voteCount', function (votes) {
+socket.on('voteCount-' + surveyId.trim(), function (votes) {
   if (countedVotes.children.length > 0) {
     while (countedVotes.firstChild) {
       countedVotes.removeChild(countedVotes.firstChild);
@@ -17,9 +19,3 @@ socket.on('voteCount', function (votes) {
     countedVotes.appendChild(node);
   }
 });
-
-for (var i = 0; i < buttons.length; i++) {
-  buttons[i].addEventListener('click', function () {
-    socket.send('voteCast', this.innerText);
-  });
-}
